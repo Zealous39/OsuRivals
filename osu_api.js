da719a9b5bd3f30dabedb2904241938373bcb438
Zealous39 <tylerg0656@icloud.com>
2026-08-30 23:30:58 -0400

    Added ability to find a users top 100 scores (not including osu!Lazer)

--- osu_api.js
+++ osu_api.js
@@ -1,10 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-async function getLegacyUserTop100(user, mode = 0, limit = 1) {
-  // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
-  const url = `https://osu.ppy.sh/api/get_user_best?k=${process.env.OSU_API_KEY}&u=${user}&m=${mode}&limit=${limit}`
-}
 async function getLegacyUserData(user, mode = 0)
 {
   // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
@@ -29,5 +25,31 @@ async function getLegacyUserData(user, mode = 0)
   }
 }
 
+async function getLegacyUserDataTop100(user, mode = 3, limit = 100)
+{
+  // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
+  const url = `https://osu.ppy.sh/api/get_user_best?k=${process.env.OSU_API_KEY}&u=${user}&m=${mode}&limit=${limit}`
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+
+    const data = await res.json();
+
+    if (data.length === 0) {
+      console.log('User not found.');
+    }
+  for (let i = 0; i < 100; i++)
+  {
+    let userDataTop100 = data[i];
+    console.log("Score: ", i + 1);
+    console.log(`beatmap_id: ${userDataTop100.beatmap_id}`);
+    console.log(`pp: ${userDataTop100.pp}`);
+    console.log("");
+  }
+  } catch (error) {
+    console.error('API Error:', error.message);
+  }
+}
+
 // Search by username or user ID:
-getLegacyUserData('Zeal_');
+getLegacyUserDataTop100('Zeal_');
