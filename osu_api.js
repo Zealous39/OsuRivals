b4e403be0bd966349356468983791b14f755aa08
Zealous39 <tylerg0656@icloud.com>
2026-08-30 22:55:54 -0400

    Fixed cross machine issues with searching for user

--- osu_api.js
+++ osu_api.js
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-async function getLegacyUser(user, mode = 0) {
+async function getLegacyUserTop100(user, mode = 0, limit = 1) {
+  // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
+  const url = `https://osu.ppy.sh/api/get_user_best?k=${process.env.OSU_API_KEY}&u=${user}&m=${mode}&limit=${limit}`
+}
+async function getLegacyUserData(user, mode = 0)
+{
   // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
   const url = `https://osu.ppy.sh/api/get_user?k=${process.env.OSU_API_KEY}&u=${user}&m=${mode}`;
-}
-async function getLegacyUser_Top100(user, mode = 0) {
-  // mode: 0 = osu!, 1 = Taiko, 2 = Catch, 3 = Mania
-  const url = `https://osu.ppy.sh/api/get_user?k=${process.env.OSU_API_KEY}&u=${user}&m=${mode}`;
-}
   try {
     const res = await fetch(url);
     if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
@@ -17,7 +17,6 @@ async function getLegacyUser_Top100(user, mode = 0) {
 
     if (data.length === 0) {
       console.log('User not found.');
-      return;
     }
 
     const userData = data[0];
@@ -28,7 +27,7 @@ async function getLegacyUser_Top100(user, mode = 0) {
   } catch (error) {
     console.error('API Error:', error.message);
   }
-
+}
 
 // Search by username or user ID:
-getLegacyUser('mrekk');
+getLegacyUserData('Zeal_');
