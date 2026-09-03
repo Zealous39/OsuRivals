bed8280e0c4bd0b9d9aa48b6572a099c431916b1
Jacob Flores <jgflores1029@gmail.com>
2026-09-02 21:24:45 -0400

    Implement models folder

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,17 @@
-import {pool} from '../database.js';
 import { getLegacyUserData } from '../osu_api.js';
+import * as UserModel from '../models/users.js'
 
-const createUser = async (User_id, User, Rank, PP) =>
-{
-    const result = await pool.query(`
-    INSERT INTO users (user_id, username, \`rank\`, pp)
-    VALUES (?, ?, ?, ?)
-    `, [User_id, User, Rank, PP])
-    return result
-}
 
 export const searchUser = async (req, res) =>{
     const username = req.body.username;
     const userData = await getLegacyUserData(username);
     const {User_id, User , Rank, PP} = userData
-    await createUser(User_id, User, Rank, PP);
+    await UserModel.createUser(User_id, User, Rank, PP)
     res.json(userData);
 }
 
 export const getAllUsers = async(req, res) => {
-    const results = await pool.query('SELECT * FROM users')
+    const results = await UserModel.getAllUsers()
     res.json(results[0])
 }
 
